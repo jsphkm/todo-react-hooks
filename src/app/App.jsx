d8fd9be9384fd33a0802aed7f2faac7f36936f7c
jsphkm <jsphkm12@gmail.com>
2019-04-03 14:11:04 -0700

    Create and configure react from scratch

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+// App.jsx
+import React from 'react';
+import style from './styles.css';
+
+const App = () => (
+  <div className={style.app}>
+    Hello, World!
+  </div>
+);
+
+export default App;
